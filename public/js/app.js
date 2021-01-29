b1525daa7b6862c95e488c648cba1912b40f260c
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-29 09:22:32 -0800

    Added support for crit

--- public/js/app.js
+++ public/js/app.js
@@ -7,9 +7,17 @@ function testAttack(){
     var numDice= damage.split("d");
     //console.log(numDice[0]);
     let numDamage=0;
+    let attackDie=Math.floor(Math.random()*20+1);
+    let crit=1;
+    if(attackDie==20){
+        crit=2;
+    }else if(attackDie==1){
+        crit=0;
+    }
     for(let i=0;i<parseInt(numDice[0]);i++){
         numDamage+=Math.floor(Math.random()*parseInt(numDice[1])+1);
     }
     document.getElementById("name").innerHTML=name;
-    document.getElementById("damage").innerHTML=numDamage;
+    document.getElementById("attack").innerHTML=attackDie;
+    document.getElementById("damage").innerHTML=numDamage*crit;
 }
